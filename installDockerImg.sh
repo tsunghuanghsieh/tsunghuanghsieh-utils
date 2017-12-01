4eb38214a7887591ec102972f68c1202a15f37cd
Tsung-Huang Hsieh <tsung.h.hsieh@gmail.com>
2017-12-01 03:06:12 -0800

    Added more to installDockerImg.sh

--- installDockerImg.sh
+++ installDockerImg.sh
@@ -3,6 +3,11 @@
 # docker binary
 DockerBinary=`which docker`
 
+if [ ! -f $DockerBinary ]; then
+    echo $DockerBinary is not installed on the system...
+    exit 1
+fi
+
 #
 # usage: script usage
 #
@@ -12,9 +17,47 @@ usage() {
     exit 1
 }
 
+DockerImageFilename=$1
+BuildEnv=$2
+
+[ $# -ne 2 ] && usage "incorrect number of arguments"
+
+if [ ! -z $DockerImageFilename ]; then
+    echo $DockerImageFilename does not exist...
+    exit 1
+fi
+
+if [ ! -z `echo $DockerImageFilename | grep .gz` ]; then
+    fileExt='.image.gz'
+else
+    fileExt='.image'
+fi
+
+DockerImagePrefix='auto_bld'
+DockerImageName=${DockerImageFilename%-*}
+DockerImageTagName=`basename ${DockerImageFilename#*-} $fileExt`
+BuildType=${DockerImageName#*$DockerImagePrefix}
+
+#
+# Functions
+#
+
 #
 RemoveContainer() {
+    # back up IFS
     LocalBakIFS=$IFS
 
     IFS=$OIFS
+
+    #
+    # CONTAINER ID|IMAGE|COMMAND|CREATED|STATUS|PORTS|NAMES
+
+    # stop and remove container
+
+    # restore IFS
+    IFS=$LocalBakIFS
 }
+
+#
+# Do execution
+#
